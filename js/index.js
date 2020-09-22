293d4b690e43db880a26b39a8eafdec32c22ac22
Emily <porte2@mailbox.winthrop.edu>
2020-09-21 20:22:00 -0400

    fixed layout of ideas phone

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector("nav > ul");
 const links = document.querySelectorAll("nav > ul > li");
+const footerLinks = document.querySelectorAll("footer > ul > li");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
-    })
+    });
+    footerLinks.forEach(flink => {
+        flink.classList.toggle("disappear");
+    });
 })
