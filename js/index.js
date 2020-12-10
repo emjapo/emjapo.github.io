0e6b088cf56e1ca9bd574a279e8ec096e86f0ba8
Emily <porte2@mailbox.winthrop.edu>
2020-12-10 04:15:43 -0500

    gsap animation added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-var tl = gsap.timeline({ repeat: 2, repeatDelay: 1 });
-tl.to("#id", { x: 100, duration: 1 });
-tl.to("#id", { y: 50, duration: 1 });
-tl.to("#id", { opacity: 0, duration: 1 });
+var tl = gsap.timeline();
+tl.to("#smile", { scale: 10, rotation:30, duration: .7, ease: 'elastic' });
+tl.to("#smile", { scale: 7, rotation:-30, duration: .6, ease: 'elastic'});
+tl.to("#portCity", { scale:-1/3, duration: .2 });
+tl.to("#smile", { scale: 1, rotation:0, duration: 1, ease: 'bounce' });
+tl.to("#portCity", { opacity: 1, scale:1, duration: 1 });
+tl.to("#studios", { y:30, opacity: 1, duration: 1.5, ease: 'sine' });
