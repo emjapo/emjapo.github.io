abd7e46d40d7768081e9f762d75e51305eaf3545
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-05-12 17:50:13 -0400

    photos page done for my screen

---
+++ js/links.js
@@ -0,0 +1,9 @@
+var projectsLink = document.getElementById("projectsLink");
+var researchLink = document.getElementById("researchLink");
+var photosLink = document.getElementById("photosLink");
+
+function MakeActiveProj() {
+    projectsLink.style.color = "#63CCC8";
+    var projects = document.getElementById("projects");
+    
+}
